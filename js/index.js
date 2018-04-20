8e9c6e00bd0d220f792822c2777db0e4ebf12fdc
elvisxb <elvisxb@aim.com>
2018-04-20 15:56:22 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,210 @@
+$('.menu-button-1').on('click', () => {
+  $('.nav-menu-1').toggle();
+});
+$('.menu-button-2').on('click', () => {
+  $('.nav-menu-2').toggle();
+});
+$('.menu-button-3').on('click', () => {
+  $('.nav-menu-3').toggle();
+});
+$('.menu-button-4').on('click', () => {
+  $('.nav-menu-4').toggle();
+});
+$('.menu-button-5').on('click', () => {
+  $('.nav-menu-5').toggle();
+});
+$('.menu-button-6').on('click', () => {
+  $('.nav-menu-6').toggle();
+});
+$('.menu-button-7').on('click', () => {
+  $('.nav-menu-7').toggle();
+});
+$('.menu-button-8').on('click', () => {
+  $('.nav-menu-8').toggle();
+});
+$('.menu-button-9').on('click', () => {
+  $('.nav-menu-9').toggle();
+});
+$('.menu-button-10').on('click', () => {
+  $('.nav-menu-10').toggle();
+});
+$('.menu-button-11').on('click', () => {
+  $('.nav-menu-11').toggle();
+});
+$('.menu-button-12').on('click', () => {
+  $('.nav-menu-12').toggle();
+});
+$('.menu-button-13').on('click', () => {
+  $('.nav-menu-13').toggle();
+});
+$('.menu-button-14').on('click', () => {
+  $('.nav-menu-14').toggle();
+});
+$('.menu-button-15').on('click', () => {
+  $('.nav-menu-15').toggle();
+});
+$('.menu-button-16').on('click', () => {
+  $('.nav-menu-16').toggle();
+});
+$('.menu-button-17').on('click', () => {
+  $('.nav-menu-17').toggle();
+});
+$('.menu-button-18').on('click', () => {
+  $('.nav-menu-18').toggle();
+});
+$('.menu-button-19').on('click', () => {
+  $('.nav-menu-19').toggle();
+});
+$('.menu-button-20').on('click', () => {
+  $('.nav-menu-20').toggle();
+});
+$('.menu-button-21').on('click', () => {
+  $('.nav-menu-21').toggle();
+});
+$('.menu-button-22').on('click', () => {
+  $('.nav-menu-22').toggle();
+});
+$('.menu-button-23').on('click', () => {
+  $('.nav-menu-23').toggle();
+});
+$('.menu-button-24').on('click', () => {
+  $('.nav-menu-24').toggle();
+});
+$('.menu-button-25').on('click', () => {
+  $('.nav-menu-25').toggle();
+});
+$('.menu-button-26').on('click', () => {
+  $('.nav-menu-26').toggle();
+});
+$('.menu-button-27').on('click', () => {
+  $('.nav-menu-27').toggle();
+});
+$('.menu-button-28').on('click', () => {
+  $('.nav-menu-28').toggle();
+});
+$('.menu-button-29').on('click', () => {
+  $('.nav-menu-29').toggle();
+});
+$('.menu-button-30').on('click', () => {
+  $('.nav-menu-30').toggle();
+});
+$('.menu-button-31').on('click', () => {
+  $('.nav-menu-31').toggle();
+});
+$('.menu-button-32').on('click', () => {
+  $('.nav-menu-32').toggle();
+});
+$('.menu-button-33').on('click', () => {
+  $('.nav-menu-33').toggle();
+});
+$('.menu-button-34').on('click', () => {
+  $('.nav-menu-34').toggle();
+});
+$('.menu-button-35').on('click', () => {
+  $('.nav-menu-35').toggle();
+});
+$('.menu-button-36').on('click', () => {
+  $('.nav-menu-36').toggle();
+});
+$('.menu-button-37').on('click', () => {
+  $('.nav-menu-37').toggle();
+});
+$('.menu-button-38').on('click', () => {
+  $('.nav-menu-38').toggle();
+});
+$('.menu-button-39').on('click', () => {
+  $('.nav-menu-39').toggle();
+});
+$('.menu-button-40').on('click', () => {
+  $('.nav-menu-40').toggle();
+});
+$('.menu-button-41').on('click', () => {
+  $('.nav-menu-41').toggle();
+});
+$('.menu-button-42').on('click', () => {
+  $('.nav-menu-42').toggle();
+});
+$('.menu-button-43').on('click', () => {
+  $('.nav-menu-43').toggle();
+});
+$('.menu-button-44').on('click', () => {
+  $('.nav-menu-44').toggle();
+});
+$('.menu-button-45').on('click', () => {
+  $('.nav-menu-45').toggle();
+});
+$('.menu-button-46').on('click', () => {
+  $('.nav-menu-46').toggle();
+});
+$('.menu-button-47').on('click', () => {
+  $('.nav-menu-47').toggle();
+});
+$('.menu-button-48').on('click', () => {
+  $('.nav-menu-48').toggle();
+});
+$('.menu-button-49').on('click', () => {
+  $('.nav-menu-49').toggle();
+});
+$('.menu-button-50').on('click', () => {
+  $('.nav-menu-50').toggle();
+});
+$('.menu-button-51').on('click', () => {
+  $('.nav-menu-51').toggle();
+});
+$('.menu-button-52').on('click', () => {
+  $('.nav-menu-52').toggle();
+});
+$('.menu-button-53').on('click', () => {
+  $('.nav-menu-53').toggle();
+});
+$('.menu-button-54').on('click', () => {
+  $('.nav-menu-54').toggle();
+});
+$('.menu-button-55').on('click', () => {
+  $('.nav-menu-55').toggle();
+});
+$('.menu-button-56').on('click', () => {
+  $('.nav-menu-56').toggle();
+});
+$('.menu-button-57').on('click', () => {
+  $('.nav-menu-57').toggle();
+});
+$('.menu-button-58').on('click', () => {
+  $('.nav-menu-58').toggle();
+});
+$('.menu-button-59').on('click', () => {
+  $('.nav-menu-59').toggle();
+});
+$('.menu-button-60').on('click', () => {
+  $('.nav-menu-60').toggle();
+});
+$('.menu-button-61').on('click', () => {
+  $('.nav-menu-61').toggle();
+});
+$('.menu-button-62').on('click', () => {
+  $('.nav-menu-62').toggle();
+});
+$('.menu-button-63').on('click', () => {
+  $('.nav-menu-63').toggle();
+});
+$('.menu-button-64').on('click', () => {
+  $('.nav-menu-64').toggle();
+});
+$('.menu-button-65').on('click', () => {
+  $('.nav-menu-65').toggle();
+});
+$('.menu-button-66').on('click', () => {
+  $('.nav-menu-66').toggle();
+});
+$('.menu-button-67').on('click', () => {
+  $('.nav-menu-67').toggle();
+});
+$('.menu-button-68').on('click', () => {
+  $('.nav-menu-68').toggle();
+});
+$('.menu-button-69').on('click', () => {
+  $('.nav-menu-69').toggle();
+});
+$('.menu-button-70').on('click', () => {
+   $('.nav-menu-70').toggle();
+ });
