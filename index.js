49a67e8cbd034da29c5da5fe7448b0a3ec206ef2
elvisxb <elvisxb@aim.com>
2018-05-01 12:50:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-$(document).ready(() => {
-
-$('.nav-menu').on('click', () => {
-  $(event.currentTarget).slideUp('fast')})
-});
-
-$('.weeks-1').on('click', () => {
-   $('.set-1').children().slideDown('fast')
+jQuery(document).ready(() => {
+  jQuery(".weeks").on("click", () => {
+    jQuery(event.currentTarget)
+      .siblings()
+      .slideDown("fast");
   });
 
-$('.weeks-2').on('click', () => {
-   $('.set-2').children().slideDown('fast');
-});
-
-$('.weeks-3').on('click', () => {
-   $('.set-3').children().slideDown('fast');
-});
-
-$('.weeks-4').on('click', () => {
-   $('.set-4').children().slideDown('fast');
-});
-
-$('.weeks-5').on('click', () => {
-   $('.set-5').children().slideDown('fast');
-});
-
-$('.weeks-6').on('click', () => {
-   $('.set-6').children().slideDown('fast');
-});
-
-$('.weeks-7').on('click', () => {
-   $('.set-7').children().slideDown('fast');
-});
-
-$('.weeks-8').on('click', () => {
-   $('.set-8').children().slideDown('fast');
-});
-
-$('.weeks-9').on('click', () => {
-   $('.set-9').children().slideDown('fast');
+  jQuery("li").on("click", () => {
+    jQuery(event.currentTarget).slideUp("fast");
+  });
 });
